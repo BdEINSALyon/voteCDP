94716988d70bcbbb07d541d0fa1c7559cdf383f5
rfournier1 <romain.fournier@insa-lyon.fr>
2019-02-14 21:08:28 +0100

    added popup and form clear for data validation

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Vote
 from .models import Votant
+from .models import Liste
 import logging
 
 logger = logging.getLogger(__name__)
@@ -23,6 +24,27 @@ class ListForm(forms.ModelForm):
                 user.save()
                 return super(ListForm, self).save(commit=commit)
 
+    def clean(self):
 
+        cleaned_data = super(ListForm, self).clean()
+
+        liste_1 = cleaned_data.get('liste_1')
+        liste_2 = cleaned_data.get('liste_2')
+        liste_3 = cleaned_data.get('liste_3')
+        liste_4 = cleaned_data.get('liste_4')
+
+        vote=[liste_1, liste_2, liste_3, liste_4]
+        if len(vote) != len(set(vote)):# si oui -> doublons
+            raise forms.ValidationError("listes identiques")
+
+        listesSet = Liste.objects.all()
+        listes = []
+        for l in listesSet:
+            listes.append(l)
+
+        if not set(vote).issubset(set(listes)):
+            raise forms.ValidationError("liste(s) inexistantes")
+
+        return cleaned_data  # N'oublions pas de renvoyer les données si tout est OK
 
 
