d4268c807e614a4eb6907842cbee36c716df5057
JeanRibes <jean.ribes@bde-insa-lyon.fr>
2019-02-15 17:16:19 +0100

    importe nom/prénom/email exporte aussi token/mail_sent/vote_ok

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export.fields import Field
+from import_export.resources import ModelResource
+
 from .models import *
 
 # Register your models here.
@@ -10,7 +14,18 @@ class ListeAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Vote)
+
+class VotantRessource(ModelResource):
+  class Meta:
+    model = Votant
+    fields = ('nom', 'prenom', 'email')
+    import_id_fields=('email',) #pour que import_export puisse trouver les nouvelles entrées
+  vote_ok = Field(attribute='vote_ok',readonly=True)
+  email_sent = Field(attribute='email_sent',readonly=True)
+  token = Field(attribute='token', readonly=True)
+
 @admin.register(Votant)
-class VotantAdmin(admin.ModelAdmin):
+class VotantAdmin(ImportExportModelAdmin):
   list_display = ('token', 'nom', 'prenom', 'vote_ok', 'email_sent')
+  resource_class = VotantRessource
 
